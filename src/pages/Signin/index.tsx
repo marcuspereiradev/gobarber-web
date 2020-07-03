6c1f8fe0b26f6dec8a121b39b9095a5c685477b9
Marcus Pereira <marcus.work@hotmail.com>
2020-07-03 18:06:07 -0300

    Changing tags to components and styles

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input/index';
+import Button from '../../components/Button/index';
 
 import { Container, Content, Background } from './styles';
 
@@ -12,9 +14,9 @@ const Signin: React.FC = () => (
 
       <form>
         <h1>Faça seu logon</h1>
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+        <Input name="email" icon={FiLock} type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
